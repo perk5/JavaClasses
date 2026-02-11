ee6e07d108bdd43205e4c5a5dd321e007447ff50
Prerak Shah <prerak9359@gmail.com>
2026-02-11 16:10:03 +1030

    Working on LinkedLists

---
+++ Game.java
@@ -0,0 +1,79 @@
+import java.util.*;
+
+class GameEntry {
+    protected String name;
+    protected int score;
+
+    GameEntry(String n, int s) {
+        this.name = n;
+        this.score = s;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public String traverse() {
+        return "(" + name + ", " + score + ")";
+    }
+}
+
+class Score {
+    public static final int maxEntries = 10;
+    protected int n;
+    protected GameEntry[] entries;
+
+    public Score() {
+        entries = new GameEntry[maxEntries];
+        n = 0;
+    }
+
+    public String traverse() {
+        String s = "[";
+        for (int i = 0; i < n; i++) {
+            if (i > 0) {
+                s += " , ";
+            }
+            s += entries[i].score;
+            
+        }
+        return s + "]";
+    }
+
+    public void add(GameEntry e) {
+        int newScore = e.getScore();
+
+        if (n == maxEntries) {
+            if (newScore <= entries[n - 1].getScore()) {
+                return;
+            }
+        } else {
+            n++;
+        }
+        int i = n - 1;
+        for (; (i >= 1) && (newScore > entries[i - 1].getScore()); i--) {
+            entries[i] = entries[i - 1];
+        }
+        entries[i] = e;
+
+    }
+}
+
+class Game {
+    public static void main(String args[]) {
+        GameEntry PS = new GameEntry("Prerak", 200);
+        GameEntry RJ = new GameEntry("Raj", 300);
+
+        // System.out.println(GE.traverse());
+
+        Score Sc = new Score();
+        Sc.add(PS);
+        Sc.add(RJ);
+        System.out.println(Sc.traverse());
+        System.out.println(Sc.n);
+    }
+}
