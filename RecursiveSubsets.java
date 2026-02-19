83a9d8155b251e5b367a857e035c8e3a88d0bbf6
Prerak Shah <prerak9359@gmail.com>
2026-02-19 12:36:55 +1030

    Completed Recursive Subsets in java.

---
+++ RecursiveSubsets.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class RecursiveSubsets {
+
+    public static void printSubsets(List<Integer> arr, List<Integer> subSets, List<List<Integer>> allSubsets, int n) {
+        if (n == arr.size()) {
+            allSubsets.add(new ArrayList<>(subSets));
+            System.out.println(allSubsets);
+            return;
+        }
+
+        subSets.add(arr.get(n));
+        printSubsets(arr, subSets, allSubsets, n + 1);
+        subSets.remove(subSets.size() - 1);
+        printSubsets(arr, subSets, allSubsets, n + 1);
+
+    }
+
+    public static void main(String args[]) {
+        List<Integer> arr = new ArrayList<>();
+        arr.add(1);
+        arr.add(2);
+        arr.add(3);
+        List<Integer> subSets = new ArrayList<>();
+        List<List<Integer>> allSubsets = new ArrayList<>();
+
+        printSubsets(arr, subSets, allSubsets, 0);
+    }
+}
